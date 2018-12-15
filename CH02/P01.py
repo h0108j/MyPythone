40902a3fa81331771b1508393cdfc3c2529b493e
h0108j <45707969+h0108j@users.noreply.github.com>
2018-12-15 16:56:31 +0900

    chapter02
    2018-12-15

---
+++ CH02/P01.py
@@ -0,0 +1,5 @@
+name = input("이름을 입력하시오")
+age = input("나이를 입력하시오")
+age = int(age)
+year = 2018+100-age
+print(name, "씨는" , year , "년에 100살이시네요.")
---
+++ CH02/P03.py
@@ -0,0 +1,3 @@
+radius = float(input("반지름을 입력하세요."))
+size = 3.14159 * radius * radius
+print("반지름이",radius,"인 원의 넓이 =",size)
