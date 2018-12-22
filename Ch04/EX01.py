a2b538a335ede121cd525902e427b7ab7fa7debe
h0108j <45707969+h0108j@users.noreply.github.com>
2018-12-22 16:55:10 +0900

    Ch04
    2018-12-22

---
+++ Ch04/EX01.py
@@ -0,0 +1,1 @@
+print("I ate " , 12 , "apples.")
---
+++ Ch04/ListTest.py
@@ -0,0 +1,23 @@
+alist = ["이순신" , 25 , "abc" , 3.5] #a리스트
+for a in alist:
+  print(a)
+
+print(alist[1:3])
+print(alist[:3])
+print(alist[1:])
+print(alist[-3:])
+print(alist[-1])
+
+alist.append(50)
+alist.append("대한민국")
+print(alist)
+
+blist = ["대한민국" , "일본" , "미국" , "중국" , "베트남"] #b리스트
+print(blist)
+blist.append("호주")
+print(blist)
+
+clist = [56 , 29 , 134 , 333] #c리스트
+clist.append(24)
+print(clist)
+print(type(clist))
