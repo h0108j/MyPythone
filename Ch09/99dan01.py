f2c99a3ee2e5cbd207ebbf4c7025a2c40f9d6c86
h0108j <45707969+h0108j@users.noreply.github.com>
2019-01-05 15:06:02 +0900

    ch09
    ch09

---
+++ Ch09/99dan01.py
@@ -0,0 +1,9 @@
+for dan in range(2, 10):
+
+  for n in range(1, 10):
+    if n == 5:
+      continue
+    print( dan, "*" , n , "=" , dan*n )
+
+  print()
+    
---
+++ Ch09/dic.py
@@ -0,0 +1,8 @@
+new_dic = {}
+new_dic["Korea"]="소능민"
+new_dic["Japan"]=["싄즤","놔까무롸"]
+new_dic["Korea"]=["소능민","이청룡"]
+print(new_dic)
+print(new_dic["Japan"])
+print(new_dic.keys())
+print(new_dic.values())
